9e3c09d626163e40f6a103cc123bea147656b9f6
Hakan Baran <hakanbaran.developer@gmail.com>
2023-03-23 09:49:33 +0300

    Fetch Profile Photo data...

--- Spotify-Clone/Controllers/Other/ProfileVC.swift
+++ Spotify-Clone/Controllers/Other/ProfileVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ProfileVC: UIViewController {
     
@@ -82,14 +83,32 @@ class ProfileVC: UIViewController {
         models.append("Email Address: \(model.email)")
         models.append("User ID: \(model.id)")
         models.append("Plan: \(model.product)")
-        
-        
-        
-        
-        
+        createTableHeader(with: model.images.first?.url)
         
         tableView.reloadData()
         
+        
+    }
+    
+    
+    private func createTableHeader(with string: String?) {
+        
+        guard let urlString = string, let url = URL(string: urlString) else {return}
+        
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: view.width/1.5))
+        
+        let imageSize : CGFloat = headerView.height / 2
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: imageSize, height: imageSize))
+        headerView.addSubview(imageView)
+        imageView.center = headerView.center
+        imageView.contentMode = .scaleAspectFill
+        
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = imageSize / 2
+        
+        imageView.sd_setImage(with: url)
+        
+        tableView.tableHeaderView = headerView
         
     }
     
