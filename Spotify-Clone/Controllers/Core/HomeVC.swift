3d9b7f2b30f7ffa8d60e5588e8cd2ed260bd7d0f
Hakan Baran <hakanbaran.developer@gmail.com>
2023-03-30 23:16:21 +0300

    HomeVC CollectionView has been created...

--- Spotify-Clone/Controllers/Core/HomeVC.swift
+++ Spotify-Clone/Controllers/Core/HomeVC.swift
@@ -8,16 +8,57 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    
+    private var collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewCompositionalLayout { sectionIndex, _ -> NSCollectionLayoutSection in
+        return Self.createSectionLayout(section: sectionIndex)
+    })
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTabSettings))
         
+        configureCollectionView()
         fetchData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
+        collectionView.frame = view.bounds
         
     }
+    
+    private func configureCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    
+    private static func createSectionLayout(section: Int) -> NSCollectionLayoutSection {
+        
+        // Item
+        
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth((1.0)), heightDimension: .fractionalHeight(1.0)))
+        
+        // Group
+        
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(120)), repeatingSubitem: item, count: 1)
+        
+        // Section
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
+        
+    }
+    
+    
+    
+    
+    
     
     func fetchData() {
         // Featured Playlists, Recommended Tracks, New Releases...
@@ -58,5 +99,24 @@ class HomeVC: UIViewController {
     }
 
 
+}
+
+extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        return cell
+    }
+    
+    
+    
+    
+    
+    
 }
 
