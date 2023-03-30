9cc03f09eafa70fb495eb08831c04f3f4c946206
Hakan Baran <hakanbaran.developer@gmail.com>
2023-03-31 00:24:02 +0300

    CollectionView Vertical group in horizontal group...

--- Spotify-Clone/Controllers/Core/HomeVC.swift
+++ Spotify-Clone/Controllers/Core/HomeVC.swift
@@ -33,6 +33,7 @@ class HomeVC: UIViewController {
         
         
         
+        
         configureCollectionView()
         view.addSubview(spinner)
         fetchData()
@@ -61,14 +62,20 @@ class HomeVC: UIViewController {
         
         item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
         
-        // Group
+        // Vertical group in horizontal group
         
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(120)), repeatingSubitem: item, count: 1)
+        let verticalGroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(390)), subitem: item, count: 3)
+        
+        let horizontalGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: .absolute(390)), subitem: verticalGroup, count: 1)
+        
+        
+        
+        
         
         // Section
         
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .continuous
+        let section = NSCollectionLayoutSection(group: horizontalGroup)
+        section.orthogonalScrollingBehavior = .groupPaging
         return section
         
     }
