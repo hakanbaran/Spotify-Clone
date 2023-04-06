55137f75754d620fa914472a5fc5eaf4e6831131
Hakan Baran <hakanbaran.developer@gmail.com>
2023-04-06 23:19:12 +0300

    ViewModels. has been created...

---
+++ Spotify-Clone/ViewModels/FeaturedPlaylistCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FeaturedPlaylistCellViewModel.swift
+//  Spotify-Clone
+//
+//  Created by Hakan Baran on 6.04.2023.
+//
+
+import Foundation
+
+struct FeaturedPlaylistCellViewModel {
+    let name: String
+    let artworkURL: URL?
+    let creatorName: String
+}
---
+++ Spotify-Clone/ViewModels/RecomendedTrackCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  RecomendedTrackCellViewModel.swift
+//  Spotify-Clone
+//
+//  Created by Hakan Baran on 6.04.2023.
+//
+
+import Foundation
+
+
+struct RecomendedTrackCellViewModel {
+    let name: String
+    let artistName: String
+    let artworkURL: URL?
+}
