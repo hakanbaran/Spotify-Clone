38fbd1fc26572d4920e1ec2ea1f3f376d519d8f6
Hakan Baran <hakanbaran.developer@gmail.com>
2023-04-15 23:34:13 +0300

    PlaylistHeaderView Design has been complate...

--- Spotify-Clone/Controllers/Other/PlaylistVC.swift
+++ Spotify-Clone/Controllers/Other/PlaylistVC.swift
@@ -16,13 +16,17 @@ class PlaylistVC: UIViewController {
         
         let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth((1.0)), heightDimension: .fractionalHeight(1.0)))
         
-        item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 2, bottom: 1, trailing: 2)
         
         
         
-        let group = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(80)), subitem: item, count: 1)
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(60)), subitem: item, count: 1)
         
         let section = NSCollectionLayoutSection(group: group)
+        
+        section.boundarySupplementaryItems = [NSCollectionLayoutBoundarySupplementaryItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.5)), elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)]
+        
+        
         
         return section
         
@@ -38,6 +42,8 @@ class PlaylistVC: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private var viewModels = [RecommendedTrackCellViewModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,18 +51,25 @@ class PlaylistVC: UIViewController {
         view.addSubview(collectionView)
         collectionView.register(RecommendedTrackCollectionViewCell.self, forCellWithReuseIdentifier: RecommendedTrackCollectionViewCell.identifier)
         collectionView.backgroundColor = .systemBackground
+        collectionView.register(PlaylistHeaderCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: PlaylistHeaderCollectionReusableView.identifier)
         collectionView.delegate = self
         collectionView.dataSource = self
         
         title = playlist.name
         view.backgroundColor = .systemBackground
         
-        APICaller.shared.getPlaylistDetails(for: playlist) { result in
-            switch result {
-            case .success(let model):
-                break
-            case .failure(let error):
-                break
+        APICaller.shared.getPlaylistDetails(for: playlist) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let model):
+                    // RecommendedTrackCellViewModel
+                    self?.viewModels = model.tracks.items.compactMap({
+                        RecommendedTrackCellViewModel(name: $0.track.name, artistName: $0.track.artists?.first?.name ?? "", artworkURL: URL(string: $0.track.album?.images.first?.url ?? ""))
+                    })
+                    self?.collectionView.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
             }
         }
     }
@@ -77,19 +90,36 @@ extension PlaylistVC: UICollectionViewDelegate, UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        return viewModels.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecommendedTrackCollectionViewCell.identifier, for: indexPath) as? RecommendedTrackCollectionViewCell else {
             return UICollectionViewCell()
         }
-        cell.backgroundColor = .red
+        cell.configure(with: viewModels[indexPath.row])
         return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: PlaylistHeaderCollectionReusableView.identifier, for: indexPath) as? PlaylistHeaderCollectionReusableView, kind == UICollectionView.elementKindSectionHeader else {
+            return UICollectionReusableView()
+        }
+        
+        
+        let headerViewModel = PlaylistHeaderViewModel(name: playlist.name, ownerName: playlist.owner.display_name, description: playlist.description, artworkURL: URL(string: playlist.images.first?.url ?? ""))
+        
+        header.configure(with: headerViewModel)
+        
+        
+        return header
+                
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
+        
+        // Play Song
     }
     
 }
---
+++ Spotify-Clone/ViewModels/PlaylistHeaderViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  PlaylistHeaderViewModel.swift
+//  Spotify-Clone
+//
+//  Created by Hakan Baran on 15.04.2023.
+//
+
+import Foundation
+
+
+struct PlaylistHeaderViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
